5bfedd0d9d5f0ce837d1c86e388a6e7b20955f65
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2023-11-20 19:54:36 +0100

    améliorer mon programme

--- jour01/job10/main.py
+++ jour01/job10/main.py
@@ -2,14 +2,14 @@ investissement = 10000
 taux = 3/100
 
 rendement = investissement*taux
-print (f"Le rendement annuel est de {rendement} ")
+print (f"En temps 1, le rendement annuel est de {rendement}")
 
-investissement = investissement + 5000
-taux = taux + 2/100
+investissement = investissement + rendement + 5000
+taux = taux + 2/100 *taux
 rendement = investissement * taux
-print (f"Le nouveau rendement annuel est de {rendement}")
+print (f"En temps 2, le nouveau rendement annuel est de {rendement}")
 
 investissement = investissement - 10/100
 taux = taux - 1/100
 rendement = investissement *taux
-print (f"Le nouveau rendement annuel est de {rendement}")
+print (f"En temps 3, Le nouveau rendement annuel est de {rendement}")
